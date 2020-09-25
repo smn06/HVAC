eb4988255b23dd4b8f72d44ce191e80af773c3e9
mosfak <mosfak2400@gmail.com>
2020-09-26 00:32:19 +0600

    Video feed added But Program Crashes

--- app/src/main/java/com/example/hvac/camera.java
+++ app/src/main/java/com/example/hvac/camera.java
@@ -4,11 +4,81 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
 public class camera extends AppCompatActivity {
+    public RecyclerView MrecyclerView;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        MrecyclerView = findViewById(R.id.recyclerview_video);
+        MrecyclerView.setHasFixedSize(true);
+        MrecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("video");
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<member, ViewHolder> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<member, ViewHolder>(
+                        member.class,
+                        R.layout.row,
+                        ViewHolder.class,
+                        reference
+                ) {
+                    @Override
+                    protected void populateViewHolder(ViewHolder viewHolder, member member, int i) {
+                        viewHolder.setVideo(getApplication(),member.getTitle(),member.getUrl());
+                    }
+                };
+        MrecyclerView.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
---
+++ app/src/main/java/com/example/hvac/member.java
@@ -0,0 +1,24 @@
+package com.example.hvac;
+
+public class member {
+    String title, url;
+    public member(){
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
